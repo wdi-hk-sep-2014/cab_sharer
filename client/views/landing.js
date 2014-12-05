1e6f6633ab6b276a0308ac834c930e0186ba8005
Stephen <stephen.parker@timehascome.org>
2014-12-05 16:57:35 +0800

    customised map colours and added custom icon

--- client/views/landing.js
+++ client/views/landing.js
@@ -1,5 +1,8 @@
+var styles = [{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]},{"featureType":"landscape","stylers":[{"color":"#f2e5d4"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"administrative","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"road"},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{},{"featureType":"road","stylers":[{"lightness":20}]}]
+
 var map = function(lat, lng) {
 var marker;
+var gpsIcon = '/images/gpsicon.png';
 
     GoogleMaps.init(
         {
@@ -10,13 +13,19 @@ var marker;
         function(){
             var mapOptions = {
                 zoom: 16,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
+                mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
             };
-            map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions); 
+            var styledMap = new google.maps.StyledMapType(styles,
+              {name: "Styled Map"});
+            map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+            map.mapTypes.set('map_style', styledMap);
+            map.setMapTypeId('map_style');
             map.setCenter(new google.maps.LatLng( lat, lng ));
                 marker = new google.maps.Marker({
                 position: new google.maps.LatLng(lat,lng),
-                map: map
+                map: map,
+                animation: google.maps.Animation.DROP,
+                icon: gpsIcon
 
             })
         }
@@ -35,4 +44,3 @@ var getPositionByBrowser = function(){
 }
 
 getPositionByBrowser();
-

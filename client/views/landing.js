8cd7ea6291ffe04651a8e531300df6f7a515c696
Stephen <stephen.parker@timehascome.org>
2014-12-08 18:15:54 +0800

    removed facebook

--- client/views/landing.js
+++ client/views/landing.js
@@ -1,7 +1,5 @@
 if (Meteor.isClient) {
-
-
-
+  
   var styles = [{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]},{"featureType":"landscape","stylers":[{"color":"#f2e5d4"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"administrative","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"road"},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{},{"featureType":"road","stylers":[{"lightness":20}]}]
 
   var map = function(lat, lng) {
@@ -46,7 +44,8 @@ if (Meteor.isClient) {
     navigator.geolocation.getCurrentPosition(c); //c for callback function
     return false;
   }
-
   getPositionByBrowser();
 
-}
+};
+
+// alert("hello");

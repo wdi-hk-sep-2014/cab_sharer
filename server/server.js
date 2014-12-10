c8a0c59aef8ffa102bef42c74c56fe6b6546ecaf
Stephen <stephen.parker@timehascome.org>
2014-12-10 20:21:18 +0800

    commented out some stuff

--- server/server.js
+++
@@ -1,8 +0,0 @@
-// var geo = new GeoCoder({
-//   geocoderProvider: "mapquest",
-//   httpAdapter: "https",
-//   apiKey: 'Fmjtd%7Cluurn16b21%2C20%3Do5-9wts54'
-// });
-// var result = geo.geocode('Cyberport Hong Kong');
-// console.log(result);
-

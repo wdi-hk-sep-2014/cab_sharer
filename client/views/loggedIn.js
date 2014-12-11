9a413cd516440bddbd373ce020af55e75bcd1578
Stephen <stephen.parker@timehascome.org>
2014-12-11 23:07:13 +0800

    commit before redesign ux branch

--- client/views/loggedIn.js
+++ client/views/loggedIn.js
@@ -7,60 +7,86 @@ Template.loggedIn.rendered = function(){
       var styles = [{
         "featureType": "water",
         "stylers": [{
-          "visibility": "on"
+          "saturation": 43
         }, {
-          "color": "#acbcc9"
-        }]
-      }, {
-        "featureType": "landscape",
-        "stylers": [{
-          "color": "#f2e5d4"
-        }]
-      }, {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#c5c6c6"
-        }]
-      }, {
-        "featureType": "road.arterial",
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#e4d7c6"
-        }]
-      }, {
-        "featureType": "road.local",
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#fbfaf7"
-        }]
-      }, {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#c5dac6"
-        }]
-      }, {
-        "featureType": "administrative",
-        "stylers": [{
-          "visibility": "on"
+          "lightness": -11
         }, {
-          "lightness": 33
+          "hue": "#0088ff"
         }]
       }, {
-        "featureType": "road"
-      }, {
-        "featureType": "poi.park",
-        "elementType": "labels",
-        "stylers": [{
-          "visibility": "on"
-        }, {
-          "lightness": 20
-        }]
-      }, {}, {
         "featureType": "road",
+        "elementType": "geometry.fill",
         "stylers": [{
-          "lightness": 20
+          "hue": "#ff0000"
+        }, {
+          "saturation": -100
+        }, {
+          "lightness": 99
+        }]
+      }, {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+          "color": "#808080"
+        }, {
+          "lightness": 54
+        }]
+      }, {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "color": "#ece2d9"
+        }]
+      }, {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "color": "#ccdca1"
+        }]
+      }, {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#767676"
+        }]
+      }, {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#ffffff"
+        }]
+      }, {
+        "featureType": "poi",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      }, {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "visibility": "on"
+        }, {
+          "color": "#b8cb93"
+        }]
+      }, {
+        "featureType": "poi.park",
+        "stylers": [{
+          "visibility": "on"
+        }]
+      }, {
+        "featureType": "poi.sports_complex",
+        "stylers": [{
+          "visibility": "on"
+        }]
+      }, {
+        "featureType": "poi.medical",
+        "stylers": [{
+          "visibility": "on"
+        }]
+      }, {
+        "featureType": "poi.business",
+        "stylers": [{
+          "visibility": "simplified"
         }]
       }]
 

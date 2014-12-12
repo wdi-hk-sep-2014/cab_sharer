caad2b111483d5034574b830b3962df1a640d3f8
Stephen <stephen.parker@timehascome.org>
2014-12-12 18:47:02 +0800

    markers populated with user names correctly due to change in closure from swapping i and index

--- client/views/loggedIn.js
+++ client/views/loggedIn.js
@@ -1,4 +1,3 @@
-// $(document).ready(console.log("this is jquery working"));
 Template.loggedIn.rendered = function(){
 
   if (Meteor.isClient) {
@@ -92,15 +91,22 @@ Template.loggedIn.rendered = function(){
 
       var map = function(lat, lng) {
         var myMarker;
-        var onlineUserArray = Meteor.users.find().fetch(); // gather online user object information
-        var i = 1;
+        // var onlineUserArray = Meteor.users.find().fetch(); // gather online user object information
+        var onlineUsersExceptMe = Meteor.users.find({_id: {$ne: Meteor.userId()}}).fetch();
+        // console.log(onlineUsersExceptMe); // gather online user object information
+        // var i = 1;
         var otherUserMarkers = [];
-        while (i < onlineUserArray.length) { // put other online users locations into an array
-          var otherUserlat = onlineUserArray[i].profile.location.lat,
-              otherUserlng = onlineUserArray[i].profile.location.lng;
-              otherUserMarkers.push([otherUserlat, otherUserlng]);
-              i++;
+        for (user in onlineUsersExceptMe) {
+          var otherUserLat = onlineUsersExceptMe[user].profile.location.lat,
+              otherUserLng = onlineUsersExceptMe[user].profile.location.lng;
+              otherUserMarkers.push([otherUserLat, otherUserLng]);
         };
+        // while (i < onlineUserArray.length) { // put other online users locations into an array
+        //   var otherUserlat = onlineUserArray[i].profile.location.lat,
+        //       otherUserlng = onlineUserArray[i].profile.location.lng;
+        //       otherUserMarkers.push([otherUserlat, otherUserlng]);
+        //       i++;
+        // };
         // console.log(otherUserMarkers);
 
 
@@ -119,12 +125,13 @@ Template.loggedIn.rendered = function(){
               mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
             };
             var locations = [];
-            var i = 1;
-            while (i < onlineUserArray.length) {
-              locations.push(onlineUserArray[i].profile.name);
+            var i = 0;
+            while (i < onlineUsersExceptMe.length) {
+              locations.push(onlineUsersExceptMe[i].profile.name);
               i++;
+              console.log(locations);
             }
-            console.log(locations);
+
             var infowindow = new google.maps.InfoWindow();
 
             var styledMap = new google.maps.StyledMapType(styles, {
@@ -154,16 +161,15 @@ Template.loggedIn.rendered = function(){
               otherUserMarker = new google.maps.Marker({
                 position: new google.maps.LatLng(otherUserMarkers[index][0], otherUserMarkers[index][1]),
                 map: map,
-                animation: google.maps.Animation.DROP,
                 icon: gpsIcon,
                 visible: true
               });
               google.maps.event.addListener(otherUserMarker, 'click', (function(otherUserMarker, i) {
                 return function() {
-                  infowindow.setContent(locations[index]);
+                  infowindow.setContent(locations[i]);
                   infowindow.open(map, otherUserMarker);
                 }
-              })(otherUserMarker, i));
+              })(otherUserMarker, index));
             };
           }
         );
@@ -182,7 +188,8 @@ Template.loggedIn.rendered = function(){
         map(lat, lng);
         var locationHash = {
           lat: lat,
-          lng: lng
+          lng: lng,
+          updatedAt: time
         };
         Meteor.users.update({_id:Meteor.userId()}, {$set:{"profile.location" : locationHash}});
         
@@ -190,11 +197,25 @@ Template.loggedIn.rendered = function(){
 
       var getPositionByBrowser = function() {
         navigator.geolocation.getCurrentPosition(afterPositionInfo, error); //afterPositionInfo for callback function, error if geolocation unsuccessful
+        
         return false;
       }
 
-      getPositionByBrowser();
+      var mapWithExistingPosition = function() {
+        var lat = Meteor.user().profile.location.lat,
+            lng = Meteor.user().profile.location.lng
+        map(lat, lng);
+        console.log("users position is less than 5 minutes old");
+      }
 
+      // console.log (Meteor.user().profile.location.);  
+      var time = Date.now();
+      if (time >= Meteor.user().profile.location.updatedAt + 1000 * 60 * 5) {
+        console.log("locationHash older than 5 minutes");
+        getPositionByBrowser();      
+      } else {
+        mapWithExistingPosition();
+      }
 
     }
   };

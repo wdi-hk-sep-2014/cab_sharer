98f8703898f74964b33aada4c9780ec959557297
Stephen <stephen.parker@timehascome.org>
2015-04-02 10:09:43 +0800

    deployed version with basic messaging system

--- server/server.js
+++ server/server.js
@@ -19,18 +19,18 @@ ServiceConfiguration.configurations.remove({
 });
 
 // PRODUCTION
-// ServiceConfiguration.configurations.insert({
-//     service: "facebook",
-//     appId: "854947564544953",
-//     secret: "68fa355766aa072af2fa6ee5c014a001"
-// });
-
-// // DEV/TEST
 ServiceConfiguration.configurations.insert({
     service: "facebook",
-    appId: "901570066549369",
-    secret: "762e72c3d411ad4b7acfb927eddc6a31"
+    appId: "854947564544953",
+    secret: "68fa355766aa072af2fa6ee5c014a001"
 });
+
+// // DEV/TEST
+// ServiceConfiguration.configurations.insert({
+//     service: "facebook",
+//     appId: "901570066549369",
+//     secret: "762e72c3d411ad4b7acfb927eddc6a31"
+// });
 
 Accounts.onCreateUser(function(options, user) {
     if (options.profile) {
@@ -39,24 +39,3 @@ Accounts.onCreateUser(function(options, user) {
     }
     return user;
 });
-
-
-
-
-// function distance(lat1, lon1, lat2, lon2, unit) {
-//     var radlat1 = Math.PI * lat1/180
-//     var radlat2 = Math.PI * lat2/180
-//     var radlon1 = Math.PI * lon1/180
-//     var radlon2 = Math.PI * lon2/180
-//     var theta = lon1-lon2
-//     var radtheta = Math.PI * theta/180
-//     var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-//     dist = Math.acos(dist)
-//     dist = dist * 180/Math.PI
-//     dist = dist * 60 * 1.1515
-//     if (unit=="K") { dist = dist * 1.609344 }
-//     if (unit=="N") { dist = dist * 0.8684 }
-//     return dist
-// }
-
-// console.log("the two points are: " + distance(22.261724, 114.128610, 22.257186, 114.132892, "K").toFixed(2) + "KM apart.");
